98ce97055e11cbd7d8ba3b626ffc6a77ff5673cc
Med Safi <safii.med76@gmail.com>
2024-02-12 00:55:53 +0100

    MainMenu

--- Assets/Scripts/UI/UserDataPanel.cs
+++ Assets/Scripts/UI/UserDataPanel.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UserDataPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TextMeshProUGUI username = null;
+    [SerializeField] TextMeshProUGUI money = null;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        //username.SetText(GameManager.GetUserName());
+        //money.SetText(GameManager.GetMOneyAsString());
     }
-}
+} 
