6f7d5654215f7ba3a422f01ec8386bb280e59fa4
Med Safi <safii.med76@gmail.com>
2024-02-12 22:55:58 +0100

    Data

--- Assets/Scripts/Menus/StartMenuController.cs
+++ Assets/Scripts/Menus/StartMenuController.cs
@@ -1,18 +1,64 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button newGameButton = null;
+    [SerializeField] Button loadGameButton = null;
+    [SerializeField] EnterUsernameModal enterUsernameModal = null;
+    [SerializeField] SaveSlotsModal saveSlotsModal = null;
+
+    private void OnEnable()
     {
-        
+        newGameButton.onClick.AddListener(HandleNewGameButtonClicked);
+        loadGameButton.onClick.AddListener(HandleLoadGameButtonClicked);
+        Events.UsernameSubmitted += HandleUsernameWasSubmitted;
+        //SaveSlotsModal.SaveSlotSelected += HandleSaveSlotSelected;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        newGameButton.onClick.RemoveListener(HandleNewGameButtonClicked);
+        loadGameButton.onClick.RemoveListener(HandleLoadGameButtonClicked);
+        Events.UsernameSubmitted -= HandleUsernameWasSubmitted;
+        //SaveSlotsModal.SaveSlotSelected -= HandleSaveSlotWasSelected;
     }
+
+    void HandleNewGameButtonClicked()
+    {
+        //saveSlotsModal.SetIsNewGame(True);
+        //saveSlotsModal.ShowModal();
+    }
+
+    void HandleLoadGameButtonClicked()
+    {
+        //saveSlotsModal.SetIsNewGame(false);
+        //saveSlotsModal.ShowMedal();
+    }
+
+    void HandleUsernameWasSubmitted(string username)
+    {
+        Dictionary<string,string> sceneData = new Dictionary<string,string>();
+        sceneData.Add("username", username);
+        NavigationManager.LoadScene(Scenes.CHOOSE_A_CAR, sceneData);
+    }
+
+    void HandleSaveSlotWasSelected(bool isNewGame,string index)
+    {
+        if (isNewGame)
+        {
+            //enterUsernameModal.ShowModal();
+            //enterUsernameModal.Init(enterUsernameModal.HideModal);
+        }
+        else
+        {
+            Dictionary<string,string> sceneData = new Dictionary<string ,string>();
+            sceneData.Add("car_index", index);
+            NavigationManager.LoadScene(Scenes.MAIN_MENU, sceneData);
+        }
+    }
+
+
+
 }
--- Assets/Scripts/Models/Modal.cs
+++ Assets/Scripts/Models/Modal.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+ using UnityEngine;
+using UnityEngine.UI;
 
 public class Modal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button scrimButton = null;
+    
+    protected virtual void OnEnable()
     {
-        
+        if(scrimButton != null) 
+        { 
+            scrimButton.onClick.AddListener(HideModal);
+        }
+    } 
+
+    protected virtual void OnDisable()
+    {
+        if(scrimButton != null)
+        {
+            scrimButton.onClick.RemoveListener(HideModal);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowModal()
     {
-        
+        gameObject.SetActive(true);
+
+    }
+
+    public void HideModal()
+    {
+        gameObject.SetActive(false);
     }
 }
